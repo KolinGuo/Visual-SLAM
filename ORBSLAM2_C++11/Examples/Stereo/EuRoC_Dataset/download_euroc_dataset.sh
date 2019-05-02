596991e574b1a7c5904de5492b0cdd7eea5e664c
Kolin Guo <kolguo@ucdavis.edu>
2019-05-02 00:38:02 -0700

    Add ORBSLAM2_C++11

---
+++ ORBSLAM2_C++11/Examples/Stereo/EuRoC_Dataset/download_euroc_dataset.sh
@@ -0,0 +1,110 @@
+# Download datasets
+echo -e "Downloading Dataset Vicon room 1 “easy” (1.1 GB)"
+wget -c http://robotics.ethz.ch/~asl-datasets/ijrr_euroc_mav_dataset/vicon_room1/V1_01_easy/V1_01_easy.zip
+
+echo -e "\nUnzipping dataset"
+unzip -q V1_01_easy.zip -d ./V1_01_easy
+
+echo -e "\nRemove zip file"
+rm -rfv V1_01_easy.zip
+
+
+echo -e "\nDownloading Dataset Vicon room 1 “medium” (637 MB)"
+wget -c http://robotics.ethz.ch/~asl-datasets/ijrr_euroc_mav_dataset/vicon_room1/V1_02_medium/V1_02_medium.zip
+
+echo -e "\nUnzipping dataset"
+unzip -q V1_02_medium.zip -d ./V1_02_medium
+
+echo -e "\nRemove zip file"
+rm -rfv V1_02_medium.zip
+
+
+echo -e "\nDownloading Dataset Vicon room 1 “difficult” (764 MB)"
+wget -c http://robotics.ethz.ch/~asl-datasets/ijrr_euroc_mav_dataset/vicon_room1/V1_03_difficult/V1_03_difficult.zip
+
+echo -e "\nUnzipping dataset"
+unzip -q V1_03_difficult.zip -d ./V1_03_difficult
+
+echo -e "\nRemove zip file"
+rm -rfv V1_03_difficult.zip
+
+
+echo -e "\nDownloading Dataset Vicon room 2 “easy” (842 MB)"
+wget -c http://robotics.ethz.ch/~asl-datasets/ijrr_euroc_mav_dataset/vicon_room2/V2_01_easy/V2_01_easy.zip
+
+echo -e "\nUnzipping dataset"
+unzip -q V2_01_easy.zip -d ./V2_01_easy
+
+echo -e "\nRemove zip file"
+rm -rfv V2_01_easy.zip
+
+
+echo -e "\nDownloading Dataset Vicon room 2 “medium” (857 MB)"
+wget -c http://robotics.ethz.ch/~asl-datasets/ijrr_euroc_mav_dataset/vicon_room2/V2_02_medium/V2_02_medium.zip
+
+echo -e "\nUnzipping dataset"
+unzip -q V2_02_medium.zip -d ./V2_02_medium
+
+echo -e "\nRemove zip file"
+rm -rfv V2_02_medium.zip
+
+
+echo -e "\nDownloading Dataset Vicon room 2 “difficult” (783 MB)"
+wget -c http://robotics.ethz.ch/~asl-datasets/ijrr_euroc_mav_dataset/vicon_room2/V2_03_difficult/V2_03_difficult.zip
+
+echo -e "\nUnzipping dataset"
+unzip -q V2_03_difficult.zip -d ./V2_03_difficult
+
+echo -e "\nRemove zip file"
+rm -rfv V2_03_difficult.zip
+
+
+echo -e "\nDownloading Dataset machine hall “easy1” (1.5 GB)"
+wget -c http://robotics.ethz.ch/~asl-datasets/ijrr_euroc_mav_dataset/machine_hall/MH_01_easy/MH_01_easy.zip
+
+echo -e "\nUnzipping dataset"
+unzip -q MH_01_easy.zip -d ./MH_01_easy
+
+echo -e "\nRemove zip file"
+rm -rfv MH_01_easy.zip
+
+
+echo -e "\nDownloading Dataset machine hall “easy2” (1.2 GB)"
+wget -c http://robotics.ethz.ch/~asl-datasets/ijrr_euroc_mav_dataset/machine_hall/MH_02_easy/MH_02_easy.zip
+
+echo -e "\nUnzipping dataset"
+unzip -q MH_02_easy.zip -d ./MH_02_easy
+
+echo -e "\nRemove zip file"
+rm -rfv MH_02_easy.zip
+
+
+echo -e "\nDownloading Dataset machine hall “medium” (1.0 GB)"
+wget -c http://robotics.ethz.ch/~asl-datasets/ijrr_euroc_mav_dataset/machine_hall/MH_03_medium/MH_03_medium.zip
+
+echo -e "\nUnzipping dataset"
+unzip -q MH_03_medium.zip -d ./MH_03_medium
+
+echo -e "\nRemove zip file"
+rm -rfv MH_03_medium.zip
+
+
+echo -e "\nDownloading Dataset machine hall “difficult1” (693 MB)"
+wget -c http://robotics.ethz.ch/~asl-datasets/ijrr_euroc_mav_dataset/machine_hall/MH_04_difficult/MH_04_difficult.zip
+
+echo -e "\nUnzipping dataset"
+unzip -q MH_04_difficult.zip -d ./MH_04_difficult
+
+echo -e "\nRemove zip file"
+rm -rfv MH_04_difficult.zip
+
+
+echo -e "\nDownloading Dataset machine hall “difficult2” (790 MB)"
+wget -c http://robotics.ethz.ch/~asl-datasets/ijrr_euroc_mav_dataset/machine_hall/MH_05_difficult/MH_05_difficult.zip
+
+echo -e "\nUnzipping dataset"
+unzip -q MH_05_difficult.zip -d ./MH_05_difficult
+
+echo -e "\nRemove zip file"
+rm -rfv MH_05_difficult.zip
+
