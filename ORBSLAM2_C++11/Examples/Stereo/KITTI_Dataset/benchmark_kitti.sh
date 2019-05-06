b2d1e2f6e9343fe62c90c082d48de2095df8fc76
Kolin Guo <kolguo@ucdavis.edu>
2019-05-05 18:31:29 -0700

    Add benchmark script | evaluation scripts

---
+++ ORBSLAM2_C++11/Examples/Stereo/KITTI_Dataset/benchmark_kitti.sh
@@ -0,0 +1,248 @@
+#!/bin/bash
+# Make sure you have gnuplot installed
+gnuplot --version
+if [ $? -ne 0 ] ; then
+  echo -e "\ngnuplot not found. Please install it via apt install first\n"
+  exit 1
+fi
+# Make sure you have ghostscript (ps2pdf) installed
+ghostscript --version
+if [ $? -ne 0 ] ; then
+  echo -e "\nghostscript not found. Please install it via apt install first\n"
+  exit 1
+fi
+# Make sure you have pdfcrop installed
+pdfcrop --version
+if [ $? -ne 0 ] ; then
+  echo -e "\npdfcrop not found. Please install it via apt install first\n"
+  exit 1
+fi
+
+SCRIPT=$(readlink -f "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+USAGE="Usage: ./benchmark_kitti.sh [c++=[0,1]] [py=[0,1]] [KITTI Dataset Sequence Numbers]\n"
+USAGE+="\tc++=[0,1] : 0 to not benchmark C++11 implementation\n"
+USAGE+="\t            and 1 otherwise\n"
+USAGE+="\t            default is 0\n"
+USAGE+="\tpy=[0,1]  : 0 to not benchmark python implementation\n"
+USAGE+="\t            and 1 otherwise\n"
+USAGE+="\t            default is 1\n"
+USAGE+="\texample   : ./benchmark_kitti.sh 00 05 10\n"
+USAGE+="\t            only benchmark 00, 05 and 10 sequence\n"
+USAGE+="\t            default is all 11 data sequences 00-10\n"
+
+BMCPLUSPLUS=false
+BMPYTHON=true
+SEQUENCELIST=$(seq -w 00 10)
+SEQUENCELISTFULL=true
+RESULTCPLUSPLUSDIR=$(realpath "$SCRIPTPATH/resultsC++")
+RESULTPYTHONDIR=$(realpath "$SCRIPTPATH/resultsPy")
+
+# Parsing argument
+if [ $# -ne 0 ] ; then
+  while [ ! -z $1 ] ; do
+    if [[ "$1" =~ ^(0[0-9]|10)$ ]] ; then
+      if [ "$SEQUENCELISTFULL" = true ] ; then
+        SEQUENCELIST="$1"
+        SEQUENCELISTFULL=false
+      else
+        SEQUENCELIST+="\n$1"
+      fi
+    elif [ "$1" = "c++=1" ] ; then
+      BMCPLUSPLUS=true
+    elif [ "$1" = "py=0" ] ; then
+      BMPYTHON=false
+    elif [[ "$1" != "c++=0" && "$1" != "py=1" ]] ; then
+      echo -e "Unknown argument: " $1
+      echo -e "$USAGE"
+      exit 1
+    fi
+    shift
+  done
+fi
+
+# Rebuild evaluate_result.cpp
+EVALSCRIPTPATH="$SCRIPTPATH/devkit/cpp"
+echo -e "Rebuilding evaluate_result..."
+cd "$EVALSCRIPTPATH"
+make clean
+make
+if [ $? -ne 0 ] ; then
+  echo -e "\nFailed to make evaluate_result... Exiting...\n"
+  exit 1
+fi
+cd "$SCRIPTPATH"
+
+# Create new sequence lists for c++ and python
+SEQUENCELISTCPLUSPLUS=""
+SEQUENCELISTPYTHON=""
+
+# Check if any benchmark result already exists
+if [ "$BMCPLUSPLUS" = true ] ; then
+  for seq in $(echo -e $SEQUENCELIST) ; do
+    if [[ -d "$RESULTCPLUSPLUSDIR/data" && -f "$RESULTCPLUSPLUSDIR/data/$seq.txt" ]] ; then
+      echo -e "\nFile \"resultsC++/data/$seq.txt\" exists"
+      echo -e "Press 'y' to overwrite it, 'n' to skip benchmark it and 'q' to quit"
+      read option
+      if [ "$option" = y ] ; then
+        rm -rfv "$RESULTCPLUSPLUSDIR/data/$seq.txt"
+        SEQUENCELISTCPLUSPLUS+="$seq\n"
+      elif [ "$option" = q ] ; then
+        exit 0
+      elif [ "$option" != n ] ; then
+        echo -e "\nUnknown command... Exiting...\n"
+        exit 1
+      fi
+    else
+      SEQUENCELISTCPLUSPLUS+="$seq\n"
+    fi
+  done
+fi
+
+if [ "$BMPYTHON" = true ] ; then
+  for seq in $(echo -e $SEQUENCELIST) ; do
+    if [[ -d "$RESULTPYTHONDIR/data" && -f "$RESULTPYTHONDIR/data/$seq.txt" ]] ; then
+      echo -e "\nFile \"resultsPy/data/$seq.txt\" exists"
+      echo -e "Press 'y' to overwrite it, 'n' to skip benchmark it and 'q' to quit"
+      read option
+      if [ "$option" = y ] ; then
+        rm -rfv "$RESULTPYTHONDIR/data/$seq.txt"
+        SEQUENCELISTPYTHON+="$seq\n"
+      elif [ "$option" = q ] ; then
+        exit 0
+      elif [ "$option" != n ] ; then
+        echo -e "\nUnknown command... Exiting...\n"
+        exit 1
+      fi
+    else
+      SEQUENCELISTPYTHON+="$seq\n"
+    fi
+  done
+fi
+
+# Echo the benchmark information
+echo -e "\n"
+echo -e "################################################################################\n"
+echo -e "\tKITTI Benchmark Information\n"
+if [ "$BMCPLUSPLUS" = true ] ; then
+  echo -ne "\t\tC++11 Benchmark sequences:\n\t\t\t"
+  for seq in $(echo -e $SEQUENCELISTCPLUSPLUS) ; do
+    echo -n "$seq "
+  done
+  echo -e "\n"
+else
+  echo -e "\t\tDon't benchmark C++11 implementation\n"
+fi
+if [ "$BMPYTHON" = true ] ; then
+  echo -ne "\t\tPython Benchmark sequences:\n\t\t\t"
+  for seq in $(echo -e $SEQUENCELISTPYTHON) ; do
+    echo -n "$seq "
+  done
+  echo -e "\n"
+else
+  echo -e "\t\tDon't benchmark Python implementation\n"
+fi
+echo -e "################################################################################\n"
+
+# Print usage
+echo -e "\n$USAGE\n"
+
+echo -e ".......... Benchmark will start in 5 seconds .........."
+sleep 5
+
+BMCPLUSPLUSLOC=$(realpath "$SCRIPTPATH/../")
+BMCPLUSPLUSCMD00_02="./stereo_kitti ../../Vocabulary/ORBvoc.txt ./KITTI00-02.yaml ./KITTI_Dataset/dataset/sequences/"
+BMCPLUSPLUSCMD03="./stereo_kitti ../../Vocabulary/ORBvoc.txt ./KITTI03.yaml ./KITTI_Dataset/dataset/sequences/03"
+BMCPLUSPLUSCMD04_12="./stereo_kitti ../../Vocabulary/ORBvoc.txt ./KITTI04-12.yaml ./KITTI_Dataset/dataset/sequences/"
+if [ "$BMCPLUSPLUS" = true ] ; then
+  # Make directory for C++11 benchmark results
+  echo -e "\nMaking new directory to hold C++11 benchmark results..."
+  mkdir -p "$RESULTCPLUSPLUSDIR/data"
+  mkdir -p "$RESULTCPLUSPLUSDIR/errors"
+  mkdir -p "$RESULTCPLUSPLUSDIR/plot_path"
+  mkdir -p "$RESULTCPLUSPLUSDIR/plot_error"
+
+  # Call C++11 implementation
+  echo -e "\nCalling C++11 implementation..."
+  cd "$BMCPLUSPLUSLOC"
+  for seq in $(echo -e $SEQUENCELISTCPLUSPLUS) ; do
+    # Run implementation
+    if [[ "$seq" =~ ^0[0-2]$ ]] ; then
+      BMCPLUSPLUSCMD00_02+="$seq"
+      $BMCPLUSPLUSCMD00_02
+    elif [[ "$seq" =~ ^03$ ]] ; then
+      $BMCPLUSPLUSCMD03
+    elif [[ "$seq" =~ ^(0[4-9]|10)$ ]] ; then
+      BMCPLUSPLUSCMD04_12+="$seq"
+      $BMCPLUSPLUSCMD04_12
+    else
+      echo -e "\nWrong sequence number $seq\n"
+      exit 1
+    fi
+    # Check return status
+    if [ $? -ne 0 ] ; then
+      echo -e "\nFailed to benchmark C++11 implementation... Exiting...\n"
+      exit 1
+    fi
+    # Move result
+    if [ -f "CameraTrajectory.txt" ] ; then
+      mv -vf "CameraTrajectory.txt" "$RESULTCPLUSPLUSDIR/data/$seq.txt"
+    else
+      echo -e "\nNo CameraTrajectory.txt found... Exiting...\n"
+      exit 1
+    fi
+  done
+  cd "$SCRIPTPATH"
+
+  # Evaluate Results
+  echo -e "\nEvaluating C++11 results..."
+  cd "$EVALSCRIPTPATH"
+  ./evaluate_result "$RESULTCPLUSPLUSDIR"
+  if [ $? -ne 0 ] ; then
+    echo -e "\nFailed to evaluate C++11 results... Exiting...\n"
+    exit 1
+  fi
+  cd "$SCRIPTPATH"
+fi
+
+if [ "$BMPYTHON" = true ] ; then
+  # Make directory for python benchmark results
+  echo -e "\nMaking new directory to hold python benchmark results..."
+  mkdir -p "$RESULTPYTHONDIR/data"
+  mkdir -p "$RESULTPYTHONDIR/errors"
+  mkdir -p "$RESULTPYTHONDIR/plot_path"
+  mkdir -p "$RESULTPYTHONDIR/plot_error"
+
+  # Call Python implementation
+  echo -e "\nCalling Python implementation..."
+
+  # Evaluate Results
+  echo -e "\nEvaluating Python results..."
+  cd "$EVALSCRIPTPATH"
+  ./evaluate_result "$RESULTPYTHONDIR"
+  if [ $? -ne 0 ] ; then
+    echo -e "\nFailed to evaluate Python results... Exiting...\n"
+    exit 1
+  fi
+  cd "$SCRIPTPATH"
+fi
+
+# Print benchmark result
+echo -e "\n\n"
+echo -e "################################################################################\n"
+echo -e "\tKITTI Benchmark Result\n"
+if [ "$BMCPLUSPLUS" = true ] ; then
+  echo -ne "\t\tC++11 Benchmark Result:\n"
+  cat "$RESULTCPLUSPLUSDIR/times.txt"
+  echo ""
+  cat "$RESULTCPLUSPLUSDIR/stats.txt"
+  echo -e "\n"
+fi
+if [ "$BMPYTHON" = true ] ; then
+  echo -ne "\t\tPython Benchmark Result:\n"
+  cat "$RESULTPYTHONDIR/times.txt"
+  echo ""
+  cat "$RESULTPYTHONDIR/stats.txt"
+  echo -e "\n"
+fi
+echo -e "################################################################################\n"
