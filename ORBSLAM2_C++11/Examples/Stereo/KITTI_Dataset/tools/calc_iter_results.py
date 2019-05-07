66c95e282a62ae15ebd4e5d00f6fcaa48735b483
Kolin Guo <kolguo@ucdavis.edu>
2019-05-07 16:10:02 -0700

    Add plotting of iteration result

--- ORBSLAM2_C++11/Examples/Stereo/KITTI_Dataset/tools/calc_iter_results.py
+++ ORBSLAM2_C++11/Examples/Stereo/KITTI_Dataset/tools/calc_iter_results.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import numpy as np
+import matplotlib.pyplot as plt
 
 def file_len(filePath):
     with open(filePath, 'r') as f:
@@ -10,12 +11,12 @@ def file_len(filePath):
 
 # Input: argv[1] = path to benchmark iteration results folder
 def main():
-    totTransError = np.empty(0, dtype = np.float64)
-    totRotError   = np.empty(0, dtype = np.float64)
-    totMedianTime = np.empty(0, dtype = np.float64)
-    totMeanTime   = np.empty(0, dtype = np.float64)
+    totTransError = []
+    totRotError   = []
+    totMedianTime = []
+    totMeanTime   = []
 
-    iterNum = len(os.listdir(sys.argv[1]))
+    iterNum = len(os.listdir(sys.argv[1])) - 1
     totStatsFile = open(sys.argv[1]+'/'+"stats.txt", 'w')
     totTimesFile = open(sys.argv[1]+'/'+"times.txt", 'w')
     # Write headers
@@ -28,6 +29,7 @@ def main():
 
     seqNum = file_len(sys.argv[1]+'/'+os.listdir(sys.argv[1])[0]+'/'+"stats.txt") - 4
     seqCount = 0
+    seqLabels = []
     while (seqCount < seqNum):
         # Initialize
         transError = np.empty(0, dtype = np.float64)
@@ -38,7 +40,7 @@ def main():
         # Go through all iterations
         seqNumWrite = 0
         for iterDir in os.listdir(sys.argv[1]):
-            if (iterDir == "stats.txt" or iterDir == "times.txt"):
+            if (iterDir == "stats.txt" or iterDir == "times.txt" or iterDir == "plots"):
                 continue
             statsFilePath = sys.argv[1]+'/'+iterDir+'/'+"stats.txt"
             timesFilePath = sys.argv[1]+'/'+iterDir+'/'+"times.txt"
@@ -56,6 +58,7 @@ def main():
                     if (i == 2+seqCount):
                         if (seqNumWrite == 0):
                             totTimesFile.write(line.split()[0])
+                            seqLabels.append(line.split()[0])
                         medianTime = np.append(medianTime, np.array(line.split()[1], dtype = np.float64))
                         meanTime   = np.append(meanTime, np.array(line.split()[2], dtype   = np.float64))
             seqNumWrite = 1
@@ -69,10 +72,10 @@ def main():
                     np.mean(meanTime)     , np.amin(meanTime)   , np.amax(meanTime)   , np.std(meanTime)    ))
 
         # Save to total result
-        totTransError = np.append(totTransError, transError)
-        totRotError   = np.append(totRotError, rotError)
-        totMedianTime = np.append(totMedianTime, medianTime)
-        totMeanTime   = np.append(totMeanTime, meanTime)
+        totTransError.append(transError)
+        totRotError.append(rotError)
+        totMedianTime.append(medianTime)
+        totMeanTime.append(meanTime)
 
         seqCount+=1
 
@@ -88,6 +91,58 @@ def main():
     totStatsFile.close()
     totTimesFile.close()
     print('Successfully calculated results from %d iterations of %d sequences' % (iterNum, seqCount))
+
+    plotSavePath = sys.argv[1]+'/plots/'
+
+    fig1, ax1 = plt.subplots()
+    ax1.set_title('Translation Error of 11 Sequences')
+    boxplot1 = ax1.boxplot(totTransError, labels=seqLabels, 
+            showmeans=True, meanprops=dict(marker='^', markerfacecolor='g', linestyle='None'))
+    boxplot1['means'][0].set_label('Means')
+    boxplot1['medians'][0].set_label('Medians')
+    ax1.legend()
+    plt.xlabel("KITTI Sequence Number")
+    plt.ylabel("Average Relative Translation Error [%]")
+    plt.grid(axis='y', alpha=0.4, color='k', linestyle='--')
+    plt.savefig(plotSavePath+'trans_error.png', dpi=1200)
+
+    fig2, ax2 = plt.subplots()
+    ax2.set_title('Rotation Error of 11 Sequences')
+    boxplot2 = ax2.boxplot(totRotError, labels=seqLabels, 
+            showmeans=True, meanprops=dict(marker='^', markerfacecolor='g', linestyle='None'))
+    boxplot2['means'][0].set_label('Means')
+    boxplot2['medians'][0].set_label('Medians')
+    ax2.legend()
+    plt.xlabel("KITTI Sequence Number")
+    plt.ylabel("Average Relative Rotation Error [deg/100m]")
+    plt.grid(axis='y', alpha=0.4, color='k', linestyle='--')
+    plt.savefig(plotSavePath+'rot_error.png', dpi=1200)
+
+    fig3, ax3 = plt.subplots()
+    ax3.set_title('Median Tracking Time of 11 Sequences')
+    boxplot3 = ax3.boxplot(totMedianTime, labels=seqLabels, 
+            showmeans=True, meanprops=dict(marker='^', markerfacecolor='g', linestyle='None'))
+    boxplot3['means'][0].set_label('Means')
+    boxplot3['medians'][0].set_label('Medians')
+    ax3.legend()
+    plt.xlabel("KITTI Sequence Number")
+    plt.ylabel("Median Tracking Time [s]")
+    plt.grid(axis='y', alpha=0.4, color='k', linestyle='--')
+    plt.savefig(plotSavePath+'median_track_time.png', dpi=1200)
+
+    fig4, ax4 = plt.subplots()
+    ax4.set_title('Mean Tracking Time of 11 Sequences')
+    boxplot4 = ax4.boxplot(totMeanTime, labels=seqLabels, 
+            showmeans=True, meanprops=dict(marker='^', markerfacecolor='g', linestyle='None'))
+    boxplot4['means'][0].set_label('Means')
+    boxplot4['medians'][0].set_label('Medians')
+    ax4.legend()
+    plt.xlabel("KITTI Sequence Number")
+    plt.ylabel("Mean Tracking Time [s]")
+    plt.grid(axis='y', alpha=0.4, color='k', linestyle='--')
+    plt.savefig(plotSavePath+'mean_track_time.png', dpi=1200)
+
+    print('Successfully generated plots from iteration results')
 
 if __name__ == "__main__":
     main()
